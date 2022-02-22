0e8ecf5db9b3ba4cc263c961be75d776c5389216
jg2018a <seojgzm@gmail.com>
2022-02-22 23:29:22 +0100

    Deploy gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const debug = process.env.NODE_ENV !== "production";module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+    }
+  },
+  assetPrefix: !debug ? 'https://anotherplanet-io.github.io/Next-React-Components/' : '',
+}
